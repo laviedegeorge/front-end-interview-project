32d636fc75f23cc31a1be93ccadeeec06b4d9bc5
laviedegeorge <laviedegeorge1292@gmail.com>
2021-01-11 15:45:50 +0100

    correct prop types issue and styling issue

--- components/GroupContainer.js
+++ components/GroupContainer.js
@@ -6,7 +6,7 @@ const GroupContainer = ({ group }) => {
   return (
     <section className="my-6 px-4 lg:px-0">
       <h2 className=" font-bold text-2xl py-4">{name}</h2>
-      <div className="flex flex-wrap justify-between">
+      <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {products.map((product) => {
           return <ItemCard key={product.id} itemData={product} />;
         })}
--- components/Loader.js
+++ components/Loader.js
@@ -5,10 +5,10 @@ const Loader = ({ text, type }) => {
   return (
     <div
       style={{ height: "50vh" }}
-      className=" bg-gray-400 flex justify-center items-center p-4"
+      className=" bg-gray-300 flex justify-center items-center rounded-md p-4 m-4"
     >
       <p
-        className={`font-bold text-base lg:text-2xl text-white rounded-md shadow-md p-4 ${
+        className={`font-bold text-base lg:text-2xl text-white rounded-md shadow-lg p-4 ${
           type === "Error" ? "bg-red-500" : "bg-blue-500"
         }`}
       >
